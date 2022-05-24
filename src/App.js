6290b90e3917019cb1a1eab3000e87e3d84b6a32
shuvar <shuvar@gmail.com>
2022-05-25 00:02:31 +0300

    yeeee

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import "./App.css";
-import React from "react";
-// import "./icons"
+import React, { useEffect } from "react";
+import "./X.png"
 
-let shapeO = "https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/128x128/plain/shape_circle.png"
-let shapeX = "https://d1nhio0ox7pgb.cloudfront.net/_img/i_collection_png/128x128/plain/navigate_cross.png";
-let tableValue = [[null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null]];
+const shapeO = "https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/128x128/plain/shape_circle.png"
+const shapeX = "https://d1nhio0ox7pgb.cloudfront.net/_img/i_collection_png/128x128/plain/navigate_cross.png";
+// const shapeX = "./X.png";
+
+
+
 function App() {
-
+  let ItableValue = [[null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null]];
+  const [tableValue, setTableValue] = React.useState(ItableValue);
   const [currentPlayer, setCurrentPlayer] = React.useState("X");
-  const [icon, setIcon] = React.useState( shapeX );
+  const [icon, setIcon] = React.useState(shapeX);
+  const [playerWon, setPlayerWon] = React.useState(null)
 
-  // console.log(currentPlayer);
-  // winCheck(currentPlayer);
 
 
   function winCheck(player) {
-    console.log(player);
+    console.log("winCheck");
+    // console.log(currentPlayer);
+
+
     if (tableValue[0][0] === player && tableValue[1][0] === player && tableValue[2][0] === player)
-      return endTheGame(player);
+      return endTheGame(tableValue[0][0]);
     if (tableValue[3][0] === player && tableValue[4][0] === player && tableValue[5][0] === player)
       return endTheGame(player);
     if (tableValue[6][0] === player && tableValue[7][0] === player && tableValue[8][0] === player)
@@ -33,32 +39,36 @@ function App() {
     if (tableValue[2][0] === player && tableValue[4][0] === player && tableValue[6][0] === player)
       return endTheGame(player);
     else return
+
   }
 
   function endTheGame(player) {
-    alert(`${player} WON!!!!!!`);
-    tableValue = [[null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null]];
-    
+    console.log("end");
+    // console.log(playerWon);
+    // setTableValue(ItableValue);
     setCurrentPlayer("X");
     setIcon(shapeX);
-  }
- 
-  
-
-  function CreateCells() {
-    let tableArray = [];
-    for (let i = 1; i < 10; i++) {
-      tableArray.push(<div key={i} className="cell" onClick={() => clickHandler(i - 1)}><img src={tableValue[i - 1][1]} alt=""></img></div>)
-    }
-    winCheck(currentPlayer);
-    return tableArray;
+    setPlayerWon(player);
+    // alert(`${player} WON!!!!!!`);
 
   }
 
-  function clickHandler(cellNum) {
-    if (tableValue[cellNum][0] === null) {
-      tableValue[cellNum][0] = currentPlayer;
-      tableValue[cellNum][1] = icon;
+  function newGame() {
+    setTableValue(ItableValue);
+    setCurrentPlayer("X");
+    setIcon(shapeX);
+    setPlayerWon(null);
+  }
+
+
+  function clickHandler(cell) {
+    if (playerWon !== null) {
+      PlayerIsWon();
+    } else {
+    if (cell[0] === null) {
+      cell[0] = currentPlayer;
+      cell[1] = icon;
+      
       if (currentPlayer === "X") {
         setCurrentPlayer("O")
         setIcon(shapeO)
@@ -66,11 +76,25 @@ function App() {
         setCurrentPlayer("X");
         setIcon(shapeX)
       }
-      
-      // console.log(tableValue);    // delete
-      // console.log(currentPlayer);   // delete  
-    };
+    }};
   };
+
+useEffect(() => {
+  if (currentPlayer === "X") {
+    winCheck("O")
+  } else winCheck("X");
+  }, [currentPlayer]);
+
+
+  function PlayerIsWon() {
+    if (playerWon !== null) {
+      return <>
+        {playerWon} won the game
+      </>
+    }};
+  
+
+  
 
   return (
     <>
@@ -79,8 +103,16 @@ function App() {
       </header>
       <main className="table-container">
         <div className="grid-container">
-          <CreateCells />
+          {tableValue?.map((sq, i) => {
+            return <div key={i} className="cell" onClick={() => clickHandler(sq,i)}>
+              {sq[1] !== null &&
+              <img src={sq[1]} alt=""></img>}
+
+            </div>
+          })}
         </div>
+        <button onClick={() => newGame()}>new game</button>
+        <div className="playerWon"><PlayerIsWon /></div>
       </main>
 
     </>
@@ -89,24 +121,3 @@ function App() {
 
 export default App;
 
-
-
-
-/*  write the name of the players
-    choose number of matches
-    start the game
-
-
-    1. create state for player 
-    2. player chooses a square
-    3. insert x to the selected square
-    4. check if the player won
-      4.1. yes - end the game
-      4.2. no - continue to other player
-
-      end the game: 
-      1. massage that the player won
-      2. clear the tableValue
-      3. 
-
-*/
